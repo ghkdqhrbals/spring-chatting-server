3e4c5079d45455d7c362c83810b35aa043559d6b
HwangboGyumin <ghkdqhrbals@naver.com>
2023-09-16 17:13:13 +0900

    [chore] change comment in SinkStream class

--- shop-user-service/src/main/java/com/example/shopuserservice/web/util/reactor/SinkStream.java
+++ shop-user-service/src/main/java/com/example/shopuserservice/web/util/reactor/SinkStream.java
@@ -4,12 +4,18 @@ import reactor.core.publisher.Sinks;
 
 import java.time.LocalDateTime;
 
+/**
+ * this class for managing sink and sink's time of creation to check if sink is not emitting data for a long time.
+ */
 public class SinkStream {
-    // sink is a class that can emit data to the subscriber.
+    /**
+     * sink is a class that can emit data to the subscriber.
+     */
     private Sinks.Many<Object> sink;
 
-    // time when sink is created
-    // using this time, we can check how long it takes to emit data to the subscriber.
-    // and scheduler can check if the sink is not emitting data for a long time.
+    /**
+     * time when sink is created. using this time, we can check how long it takes to emit data to the subscriber.
+     * and scheduler can check if the sink is not emitting data for a long time.
+     */
     private LocalDateTime createdAt;
 }

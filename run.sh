f936a280d3c560004dbd89959ac34a658154503e
HwangboGyumin <ghkdqhrbals@naver.com>
2023-09-24 06:06:03 +0900

    [dev] pulling version postfix

--- run.sh
+++ run.sh
@@ -2,17 +2,14 @@
 
 REPOSITORY_URL=${1}
 TAG=${2}
-PREFIX="spring-chatting_"
 
 # ECR 로그인
 $(aws ecr get-login --no-include-email --region ap-northeast-2)
 
 # 해당 ECR 리포지토리의 이미지 목록 가져오기
-IMAGES=$(aws ecr list-images --repository-name chat --query 'imageIds[?starts_with(imageTag, `'$PREFIX'`)].imageTag' --output text)
-
-# 각 이미지에 대해 pull 명령어 실행
-for IMAGE in $IMAGES; do
-    docker pull ${REPOSITORY_URL}/${IMAGE}:${TAG}
+images_to_pull=$(aws ecr list-images --repository-name chat --filter "tagStatus=TAGGED" --query "imageIds[?ends_with(imageTag, '=${TAG}')].imageTag" --output text)
+for image in $images_to_pull; do
+  docker pull $ECR_URL/chat:$image
 done
 
 echo "2. Remove Dangling Docker Images";

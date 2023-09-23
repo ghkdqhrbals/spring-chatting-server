ba6a19eef70e140c0ca800da91d04783daf686b8
HwangboGyumin <ghkdqhrbals@naver.com>
2023-09-24 04:10:11 +0900

    [dev] CI/CD with ECR and EC2 autoconfiguring

--- run.sh
+++ run.sh
@@ -1,24 +1,19 @@
-#!bin/bash
+#!/bin/bash
 
-inputValue="$1"
+REPOSITORY_URL=${1}
+TAG=${2}
+PREFIX="spring-chatting_"
 
-# deafult value
-defaultValue="0.0.1"
+# ECR 로그인
+$(aws ecr get-login --no-include-email --region ap-northeast-2)
 
-# if inputValue is empty, set the default value
-if [ -z "$inputValue" ]; then
-    inputValue="$defaultValue"
-fi
+# 해당 ECR 리포지토리의 이미지 목록 가져오기
+IMAGES=$(aws ecr list-images --repository-name chat --query 'imageIds[?starts_with(imageTag, `'$PREFIX'`)].imageTag' --output text)
 
-echo "0. Remove old build files older than 30 days"
-# find every build directories which are older than 30 days and remove them
-find . -type d -name build -exec find {} -type f -mtime +30 -exec rm -f {} \; \;
-# remove empty build directories
-find . -type d -name build -exec find {} -type d -empty -delete \;
-
-echo "1. Build project with version ${inputValue}";
-
-./gradlew build --build-cache --parallel -Pversion=${inputValue} || { echo "Gradle build failed"; exit 1; }
+# 각 이미지에 대해 pull 명령어 실행
+for IMAGE in $IMAGES; do
+    docker pull ${REPOSITORY_URL}/${IMAGE}:${TAG}
+done
 
 echo "2. Remove Dangling Docker Images";
 
@@ -26,4 +21,4 @@ sh remove_dangling_image.sh
 
 echo "3. Run Server and DB Container";
 
-docker compose -f docker-compose-prod.yaml up -d --build
+docker compose -f docker-compose-prod.yaml up -d

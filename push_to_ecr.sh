536da2c260d003389b9d5cc0b8ff7caa661bb33a
HwangboGyumin <ghkdqhrbals@naver.com>
2023-09-24 17:13:19 +0900

    [chore] edit ECR URL

--- push_to_ecr.sh
+++ push_to_ecr.sh
@@ -9,7 +9,7 @@ images=$(docker images --format "{{.Repository}}" | grep "^spring-chatting-serve
 # tagging and push to ECR
 for image in $images; do
   IMAGE_NAME=$(echo "$image" | cut -d'_' -f2)_
-  ecr_image="$ECR_URL/chat:$IMAGE_NAME$NEW_VERSION"
+  ecr_image="$ECR_URL:$IMAGE_NAME$NEW_VERSION"
   docker tag "$image" "$ecr_image"
   docker push "$ecr_image"
 done

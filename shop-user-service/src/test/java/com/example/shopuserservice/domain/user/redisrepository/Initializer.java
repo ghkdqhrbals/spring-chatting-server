8114bd4eeaeb888edb68efca6cd27e48749f6549
HwangboGyumin <ghkdqhrbals@naver.com>
2023-09-28 12:37:36 +0900

    [dev] configure embeddedRedis

--- shop-user-service/src/test/java/com/example/shopuserservice/domain/user/redisrepository/Initializer.java
+++ shop-user-service/src/test/java/com/example/shopuserservice/domain/user/redisrepository/Initializer.java
@@ -1,33 +1,42 @@
 package com.example.shopuserservice.domain.user.redisrepository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import redis.embedded.RedisServer;
 
 import java.io.IOException;
 
+import static com.example.shopuserservice.web.util.server.PortChecker.isPortAvailable;
+
+@Slf4j
 public class Initializer {
     private static RedisServer redisServer;
-    private static final int redisPort=6379;
-
+    public static int redisPort;
     @BeforeAll
     public static void startRedis() throws IOException {
-        if (redisServer == null || !redisServer.isActive()) {
-            redisServer = new RedisServer(redisPort);
-            redisServer.start();
-            System.out.println("Local Redis Server Start");
-        }else{
-            System.out.println("Local Redis Server Already Running");
+        log.trace("startRedis method start");
+        for (int port = 10000; port <= 65535; port++) {
+            log.trace("Check Port: " + port);
+            if (isPortAvailable(port)) {
+                log.trace("Found Available Port: " + port);
+                redisPort = port;
+                redisServer = new RedisServer(redisPort);
+                redisServer.start();
+                log.trace("Local Redis Server is Started");
+                break;
+            }else{
+                log.trace("Check another port");
+            }
         }
     }
-
     @AfterAll
     public static void stopRedis() {
         if (redisServer != null && redisServer.isActive()) {
             redisServer.stop();
-            System.out.println("Local Redis Server Stop");
+            log.trace("Local Redis Server Stop");
         }else{
-            System.out.println("Local Redis Server Already Stopped");
+            log.trace("Local Redis Server Already Stopped");
         }
     }
 }

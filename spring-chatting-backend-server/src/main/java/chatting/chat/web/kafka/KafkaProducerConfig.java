6ca4c5c5eef1e1610fec2cc4c03c4d7f8b62c304
HwangboGyumin <ghkdqhrbals@naver.com>
2023-09-30 18:04:35 +0900

    [dev] reduce resource usage with lower tomcat threads etc.

--- spring-chatting-backend-server/src/main/java/chatting/chat/web/kafka/KafkaProducerConfig.java
+++ spring-chatting-backend-server/src/main/java/chatting/chat/web/kafka/KafkaProducerConfig.java
@@ -22,14 +22,24 @@ public class KafkaProducerConfig {
 
     @Value("${kafka.bootstrap}")
     private String bootstrapServer;
-    private final String batchSize = "50";
+    private final String batchSize = Integer.toString(32*1024); // 32KB
+    private final Integer linger = 50; // 50ms
 
     private ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
+        // Idempotence setting that let the retried messages will have same message id
+        configProps.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        // By idempotence setting, retried messages will have same message id
+        configProps.put(ProducerConfig.RETRIES_CONFIG,5);
+        // Producer cannot resend messages to another leader if the message is having same message id
+        configProps.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 1);
+        // Message batch process using batchSize and linger.ms
+        configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize); // 32KB
+        configProps.put(ProducerConfig.LINGER_MS_CONFIG, linger); // 50ms
+        DefaultKafkaProducerFactory<String, Object> pf = new DefaultKafkaProducerFactory<>(configProps);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 

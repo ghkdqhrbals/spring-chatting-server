eea33e9ef71092450374e8d3a11ab23a296cf2bf
HwangboGyumin <ghkdqhrbals@naver.com>
2023-10-05 16:20:36 +0900

    [dev] dangling image 제거 프로세스 개선

--- remove_dangling_image.sh
+++ remove_dangling_image.sh
@@ -1,11 +1,11 @@
-#!bin/bash
+#!/bin/bash
 
 echo "Dangling Image Removal Process Start";
 
 echo "1. Dangling docker image check";
-dockerDanglingArray=$(docker image list --filter "dangling=true" -q)
+dockerDanglingArray=($(docker image list --filter "dangling=true" -q))
 
-echo "${dockerDanglingArray}"
+echo "${dockerDanglingArray[*]}"
 
 echo "2. Remove empty image string"
 
@@ -13,17 +13,17 @@ echo "2. Remove empty image string"
 NEW=()
 
 # If empty string has been found, skip that index
-for i in ${dockerDanglingArray}; do
+for i in "${dockerDanglingArray[@]}"; do
    # Skip null items
    if [ -z "$i" ]; then
      continue
    fi
    # Add the rest of the elements to an array
-   NEW+=("${i}")
+   NEW+=("$i")
 done
 
 # 빈 공백이 제거된 이미지 리스트로 초기화
-dockerDanglingArray=${NEW[@]}
+dockerDanglingArray=("${NEW[@]}")
 
 # Dangling image length
 len=${#dockerDanglingArray[@]}
@@ -32,6 +32,5 @@ if [ $len -eq 0 ]; then
   echo "No Dangling Images";
 else
   echo "3. Remove dangling docker image";
-  docker rmi ${dockerDanglingArray[@]} # repository=<none> image removal start
+  docker rmi "${dockerDanglingArray[@]}" # repository=<none> image removal start
 fi
-

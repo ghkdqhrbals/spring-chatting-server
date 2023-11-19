307bf01df46ca8fbacbb94ecaafb20c250705c73
HwangboGyumin <ghkdqhrbals@naver.com>
2023-11-20 04:11:11 +0900

    [dev] redis test container

--- shop-user-service/src/test/java/com/example/shopuserservice/domain/user/redisrepository/Initializer.java
+++ shop-user-service/src/test/java/com/example/shopuserservice/domain/user/redisrepository/Initializer.java
@@ -1,44 +1,13 @@
 package com.example.shopuserservice.domain.user.redisrepository;
 
+import com.example.shopuserservice.domain.user.config.TestContainerConfig;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import redis.embedded.RedisServer;
-
-import java.io.IOException;
-
-import static com.example.shopuserservice.web.util.server.PortChecker.isPortAvailable;
 
 @Slf4j
+@ExtendWith(TestContainerConfig.class)
 @SpringBootTest
 public class Initializer {
-    private static RedisServer redisServer;
-    public static int redisPort;
-    @BeforeAll
-    public static void startRedis() throws IOException {
-        log.trace("startRedis method start");
-        for (int port = 10000; port <= 65535; port++) {
-            log.trace("Check Port: " + port);
-            if (isPortAvailable(port)) {
-                log.trace("Found Available Port: " + port);
-                redisPort = port;
-                redisServer = new RedisServer(redisPort);
-                redisServer.start();
-                log.trace("Local Redis Server is Started");
-                break;
-            }else{
-                log.trace("Check another port");
-            }
-        }
-    }
-    @AfterAll
-    public static void stopRedis() {
-        if (redisServer != null && redisServer.isActive()) {
-            redisServer.stop();
-            log.trace("Local Redis Server Stop");
-        }else{
-            log.trace("Local Redis Server Already Stopped");
-        }
-    }
+
 }

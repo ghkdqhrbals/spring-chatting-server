3e18dbf591c5f63ad5973c9c17438af4d899d4ae
HwangboGyumin <ghkdqhrbals@naver.com>
2023-11-27 20:30:33 +0900

    [fix] EnableKafka anotation location change

--- spring-chatting-backend-server/src/main/java/chatting/chat/web/kafka/KafkaAdminConfig.java
+++ spring-chatting-backend-server/src/main/java/chatting/chat/web/kafka/KafkaAdminConfig.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 @Profile("prod")
 public class KafkaAdminConfig {
 
